1089af1e4cba138545c586d56a73d217737bf1e8
Fedor Yakushev <fedor.yakushev92@mail.ru>
2022-05-25 20:30:48 +0300

    done PP task 1.1.3

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    private final static String HOST = "jdbc:mysql://localhost:3306/user";
+    private final static String USERNAME = "root";
+    private final static String PASSWORD = "466539";
+
+    private static Connection connection;
+
+    public static Connection getConnection() {
+
+        try {
+            connection = DriverManager.getConnection(HOST, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
